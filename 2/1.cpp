4aff83dcf6b20149b426b2e5cbb317b2dc391320
Lars Funke <lars.funke@udo.edu>
2018-04-26 19:42:35 +0200

    A1 lösung komplett, muss noch schön und so

--- 2/1.cpp
+++ 2/1.cpp
@@ -1,17 +1,42 @@
 #include <iostream>
 #include <fstream>
+#include <limits>
 
-//#include <strin
-#define RNGTYPE uint32_t
+class RNG
+{
+  public:
+    virtual double getRndDouble() = 0;
+};
 
 
-template <typename T>
-class LCG
+class XORShift : public RNG
 {
 public:
-  LCG(T r0, T a, T c, T m) : r(r0), a(a), c(c), m(m) {};
+  XORShift(uint16_t x, uint16_t a, uint16_t b, uint16_t c) : x(x), a(a), b(b), c(c) {};
 
-  T getRnd()
+  uint16_t getRnd()
+  {
+    x ^= (x << a);
+    x ^= (x >> b);
+    x ^= (x << c);
+    return x;
+  }
+
+  double getRndDouble()
+  {
+    return getRnd() / (double) std::numeric_limits<uint16_t>::max();
+  }
+
+private:
+  uint16_t x, a, b, c;
+};
+
+class LCG : public RNG
+{
+public:
+  LCG(uint32_t r0, uint32_t a, uint32_t c, uint32_t m) : r(r0), a(a), c(c), m(m) {};
+
+  uint32_t getRnd()
   {
     r = (a * r + c) % m;
     return r;
@@ -23,28 +48,54 @@ public:
   };
 
 private:
-  T r, a, c, m;
+  uint32_t r, a, c, m;
 };
 
-template <typename T>
-void testLCG(LCG<T>& lcg, size_t samples, std::string filename)
+int testPeriod(XORShift& rng)
+{
+  uint16_t first = rng.getRnd();
+  int i = 1;
+  for(; rng.getRnd() != first; i++);
+  return i;
+}
+
+void testRNG(RNG& rng, size_t samples, std::string filename)
 {
   std::ofstream f;  //file to save results to
 	f.open(filename); //file to save results to
   for(size_t i = 0; i < samples; i++)
-    f << lcg.getRndDouble() << std::endl;
+    f << rng.getRndDouble() << std::endl;
   f.close();
 }
 
 int main()
 {
-  LCG<RNGTYPE> lcg1(1234,      20,    120, 6075);
-  LCG<RNGTYPE> lcg2(1234,      137,   187, 256);
-  LCG<RNGTYPE> lcg3(123456789, 65539, 0,   2147483648);
-  LCG<RNGTYPE> lcg4(1234,      16807, 0,   2147483647);
-  testLCG<RNGTYPE>(lcg1, 1e5, "build/1.txt");
-  testLCG<RNGTYPE>(lcg2, 1e5, "build/2.txt");
-  testLCG<RNGTYPE>(lcg3, 1e5, "build/3.txt");
-  testLCG<RNGTYPE>(lcg4, 1e5, "build/4.txt");
+  LCG lcg1(1234,      20,    120, 6075);
+  LCG lcg2(1234,      137,   187, 256);
+  LCG lcg3(123456789, 65539, 0,   2147483648);
+  LCG lcg4(1234,      16807, 0,   2147483647);
+  XORShift xs1(123, 11, 1, 7);
+  XORShift xs2(123, 11, 4, 7);
+  testRNG(lcg1, 1e5, "build/1.txt");
+  testRNG(lcg2, 1e5, "build/2.txt");
+  testRNG(lcg3, 1e5, "build/3.txt");
+  testRNG(lcg4, 1e5, "build/4.txt");
+  testRNG(xs1,  1e5, "build/5.txt");
+  testRNG(xs2,  1e5, "build/6.txt");
+
+  std::ofstream f;  //file to save results to
+	f.open("build/period.txt"); //file to save results to
+  for(int b = 1; b <= 15; b++)
+  {
+    for(int c = 1; c <= 15; c++)
+    {
+      XORShift xs(123, 11, b, c);
+      //f << "b" << b << " c" << c << " " << testPeriod(xs) << " | ";
+      f << testPeriod(xs) << " ";
+    }
+    f << std::endl;
+  }
+  f.close();
+
   return EXIT_SUCCESS;
 }
--- 2/1.py
+++ 2/1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 
 def plot(filename, period):
     numbers = np.genfromtxt(filename+'.txt')
@@ -11,7 +12,19 @@ def plot(filename, period):
     plt.savefig(filename+'_scatter.pdf')
     plt.close()
 
-plot('build/1', 6075)
-plot('build/2', 256)
-plot('build/3', 2147483648)
-plot('build/4', 2147483647)
+#plot('build/1', 6075)
+#plot('build/2', 256)
+#plot('build/3', 2147483648)
+#plot('build/4', 2147483647)
+#plot('build/5', 100000)
+#plot('build/6', 100000)
+
+data = np.genfromtxt('build/period.txt', unpack=True)
+x = np.arange(0.5, 16.5)
+plt.pcolormesh(x, x, data, norm=colors.LogNorm(vmin=data.min(), vmax=data.max()))
+plt.xlabel('b')
+plt.ylabel('c')
+plt.colorbar(label='period')
+plt.tight_layout(pad=0)
+plt.savefig('build/period.pdf')
+plt.close()
