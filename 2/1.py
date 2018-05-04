fa501f6526ee524b57d2a766ca3c9e1fe603fd0b
Lars Funke <lars.funke@udo.edu>
2018-05-04 16:03:18 +0200

    abgabe so?

--- 2/1.py
+++ 2/1.py
@@ -1,23 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
+plt.rcParams["figure.figsize"] = (7,3)
 
 def plot(filename, period):
     numbers = np.genfromtxt(filename+'.txt')
     plt.hist(numbers, bins=10)
+    plt.xlabel('Zufallszahl')
+    plt.ylabel('#')
+    plt.tight_layout(pad=0)
     plt.savefig(filename+'_hist.pdf')
     plt.close()
 
     plt.plot(numbers[:min(period, numbers.shape[0])-1], numbers[1:min(period, numbers.shape[0])], '.', markersize=2)
+    plt.tight_layout(pad=0)
     plt.savefig(filename+'_scatter.pdf')
     plt.close()
 
-#plot('build/1', 6075)
-#plot('build/2', 256)
-#plot('build/3', 2147483648)
-#plot('build/4', 2147483647)
-#plot('build/5', 100000)
-#plot('build/6', 100000)
+plot('build/1', 6075)
+plot('build/2', 256)
+plot('build/3', 2147483648)
+plot('build/4', 2147483647)
+plot('build/5', 100000)
+plot('build/6', 100000)
 
 data = np.genfromtxt('build/period.txt', unpack=True)
 x = np.arange(0.5, 16.5)
