df273cea465117a67292ed2b848fca9183125767
Lars Funke <lars.funke@udo.edu>
2018-05-11 13:23:23 +0200

    more golfing

--- 3/1.cpp
+++ 3/1.cpp
@@ -1,26 +1,21 @@
-//CP Blatt 2, Aufgabe 1. Dag-Björn Hering und Lars Funke
-//Single spin Metropolis
-
-#include <iostream>
+//CP Blatt 3, Aufgabe 1. Dag-Björn Hering und Lars Funke: Single spin Metropolis
 #include <fstream>
 #include <random>
 
-//simulate single spin in magnetic field H, return magnetization = <s>
-double ising0d(std::mt19937& rng, unsigned int samples, double H)
+//simulate single spin in magnetic field at β = 1, return magnetization m = <s>
+double ising0d(std::mt19937& rng, unsigned int samples, double magnetic_field)
 {
-  int accumulator = 0;
   std::discrete_distribution<> dist_initial_spin ({-1, 1});
-  std::uniform_real_distribution<> dist (0, 1);
-
-  //initialize spin randomly
-  int s = dist_initial_spin(rng);
+  std::uniform_real_distribution<> dist_uniform (0, 1);
+  int accumulator = 0;
+  int spin = dist_initial_spin(rng); //initialize spin randomly
 
   for(size_t i = 0; i < samples; i++){
-    accumulator += s; //add up current spin for averaging
-    double delta_E = s * 2 * H;
+    accumulator += spin; //add up every round's spin for averaging
+    double delta_E = 2 * spin * magnetic_field;
 
-    //flip spin if ΔE≤0 or random condition is met
-    s *= delta_E <= 0 || dist(rng) < exp(-delta_E) ? -1 : 1;
+    //flip spin if ΔE<0 or random condition is met
+    spin *= delta_E < 0 || dist_uniform(rng) < exp(-delta_E)? -1 : 1;
   }
 
   return (double) accumulator / samples; //calculate mean
@@ -29,11 +24,10 @@ double ising0d(std::mt19937& rng, unsigned int samples, double H)
 int main()
 {
   std::mt19937 rng;
-	rng.seed(std::random_device()());
-  std::ofstream f;
-  f.open("build/ising0d.txt");
+  std::ofstream f("build/ising0d.txt");
 
-  //iterate through different magnetic field strenghts
-  for(double H = -5; H <= 5; H += (double) 10 / 1e3) //1e3 steps are enough
+  //iterate through different magnetic field strengths
+  for(double H = -5; H <= 5; H += 10.0 / 1e3) //1e3 steps are enough
     f << H << " " << ising0d(rng, 1e4, H) << std::endl; //1e4 samples are enough
+  return EXIT_SUCCESS;
 }
