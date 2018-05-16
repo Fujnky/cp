468cd7b3958865f691db106977b6c78222a3d007
dagbjoern <dag.hering@tu-dortmund.de>
2018-05-16 14:43:46 +0200

    Lars du faule Sau :DDDD

--- 3/2.cpp
+++ 3/2.cpp
@@ -37,8 +37,8 @@ arma::Mat<int> ising2d(std::mt19937& rng, unsigned int samples, size_t size, dou
     int i = dist_spin_flip(rng);
     int j = dist_spin_flip(rng);
 
-    spins.at(i, j) *= -1;
     double delta_E = proximity_energy_difference(spins, i , j);//energy(spins) - E;
+    spins.at(i, j) *= -1;
     //std::cout << "delta_E="<<delta_E << std::endl;
     //keep spin flipped if ΔE<0 or random condition is met
     spins.at(i, j) *= delta_E < 0 || dist_uniform(rng) < exp(-delta_E / kBT)? 1 : -1;
@@ -53,7 +53,7 @@ int main()
   rng.seed(std::random_device()());
   std::ofstream f("build/ising2d.txt");
 
-  //ising2d(rng, 1e8, 100, 1, 0).save("build/momentaufnahme.mat", arma::arma_ascii);
-  cout << geildulo(101, 100);
+  ising2d(rng, 1e8, 100, 1, 0).save("build/momentaufnahme.mat", arma::arma_ascii);
+  // cout << geildulo(101, 100);
   return EXIT_SUCCESS;
 }
