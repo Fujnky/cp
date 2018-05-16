526d1a6a6c675188eeb81c2acfbc02ecb33abc88
Lars Funke <lars.funke@udo.edu>
2018-05-16 14:03:01 +0200

    nichtsogeildulo

--- 3/2.cpp
+++ 3/2.cpp
@@ -9,22 +9,15 @@
 
 using std::cout;
 
-double energy(arma::Mat<int>& spins)
+int geildulo(int arg1, int arg2)
 {
-  double energy = 0;
-  double J = 1;
-  for(int i = 0; i < spins.n_rows; i++)
-  for(int j = 0; j < spins.n_cols; j++)
-  {
-    /*energy +=  -J* spins.at((i+1) % spins.n_cols, (j  ) % spins.n_rows) * spins.at((i-1) % spins.n_cols, (j  ) % spins.n_rows);
-    energy +=  -J* spins.at((i+1) % spins.n_cols, (j  ) % spins.n_rows) * spins.at((i  ) % spins.n_cols, (j-1) % spins.n_rows);
-    energy +=  -J* spins.at((i+1) % spins.n_cols, (j  ) % spins.n_rows) * spins.at((i  ) % spins.n_cols, (j+1) % spins.n_rows);
-    energy +=  -J* spins.at((i-1) % spins.n_cols, (j  ) % spins.n_rows) * spins.at((i  ) % spins.n_cols, (j-1) % spins.n_rows);
-    energy +=  -J* spins.at((i-1) % spins.n_cols, (j  ) % spins.n_rows) * spins.at((i  ) % spins.n_cols, (j+1) % spins.n_rows);
-    energy +=  -J* spins.at((i  ) % spins.n_cols, (j-1) % spins.n_rows) * spins.at((i  ) % spins.n_cols, (j+1) % spins.n_rows);*/
-    energy += J * spins.at(i, j) * (spins.at(i+1, j) + spins.at(i-1, j) + spins.at(i, j+1) + spins.at(i, j-1));
-  }
-  return energy;
+  return (arg1 % arg2 + arg2) % arg2;
+}
+
+double proximity_energy_difference(arma::Mat<int>& spins, int i, int j)
+{
+  constexpr double J = 1;
+  return J * spins.at(i, j) * (spins.at(geildulo(i+1, spins.n_cols), j) + spins.at(geildulo(i-1, spins.n_cols), j) + spins.at(i, geildulo(j+1, spins.n_rows)) + spins.at(i, geildulo(j-1, spins.n_rows)));
 }
 
 arma::Mat<int> ising2d(std::mt19937& rng, unsigned int samples, size_t size, double kBT, int init)
@@ -37,15 +30,15 @@ arma::Mat<int> ising2d(std::mt19937& rng, unsigned int samples, size_t size, dou
   for(auto& spin : spins)
     spin = init == 0? (dist_uniform(rng) < 0.5 ? -1 : 1) : init;
 
-  cout << spins.at(-1, -1) << std::endl;
-  double E = energy(spins);
+  //cout << spins.at(-1, -1) << std::endl;
+  //double E = pro(spins);
 
   for(size_t k = 0; k < samples; k++){
     int i = dist_spin_flip(rng);
     int j = dist_spin_flip(rng);
 
     spins.at(i, j) *= -1;
-    double delta_E = energy(spins) - E;
+    double delta_E = proximity_energy_difference(spins, i , j);//energy(spins) - E;
     //std::cout << "delta_E="<<delta_E << std::endl;
     //keep spin flipped if ΔE<0 or random condition is met
     spins.at(i, j) *= delta_E < 0 || dist_uniform(rng) < exp(-delta_E / kBT)? 1 : -1;
@@ -54,13 +47,13 @@ arma::Mat<int> ising2d(std::mt19937& rng, unsigned int samples, size_t size, dou
   return spins;
 }
 
-
 int main()
 {
   std::mt19937 rng;
   rng.seed(std::random_device()());
   std::ofstream f("build/ising2d.txt");
 
-  ising2d(rng, 1e4, 100, 0.1, 0).save("build/momentaufnahme.mat", arma::arma_ascii);
+  //ising2d(rng, 1e8, 100, 1, 0).save("build/momentaufnahme.mat", arma::arma_ascii);
+  cout << geildulo(101, 100);
   return EXIT_SUCCESS;
 }
