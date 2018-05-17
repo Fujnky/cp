c56d3cdd983ee77c6b9c80038c3f02502f1afde9
Lars Funke <lars.funke@udo.edu>
2018-05-17 15:13:51 +0200

    wer andern eine schleife schreibt, kommt selbst nie wieder heraus

--- 3/2.cpp
+++ 3/2.cpp
@@ -14,34 +14,60 @@ int geildulo(int arg1, int arg2)
   return (arg1 % arg2 + arg2) % arg2;
 }
 
-double proximity_energy_difference(arma::Mat<int>& spins, int i, int j)
+double proximity_energy(arma::Mat<double>& spins, int i, int j)
 {
   constexpr double J = 1;
+  if(geildulo(i-1, spins.n_cols) < 0 )
+    cout << "go kill me" << geildulo(i+1, spins.n_cols);
+
+
   return J * spins.at(i, j) * (spins.at(geildulo(i+1, spins.n_cols), j) + spins.at(geildulo(i-1, spins.n_cols), j) + spins.at(i, geildulo(j+1, spins.n_rows)) + spins.at(i, geildulo(j-1, spins.n_rows)));
 }
 
-arma::Mat<int> ising2d(std::mt19937& rng, unsigned int samples, size_t size, double kBT, int init)
+arma::Mat<double> ising2d(std::mt19937& rng, unsigned int samples, size_t size, double kBT, int init, arma::Mat<double>* history, arma::subview_row<double>* last)
 {
-  std::uniform_int_distribution<> dist_spin_flip (0, 100);
+  std::uniform_int_distribution<> dist_spin_flip (0, size-1);
   std::uniform_real_distribution<> dist_uniform (0, 1);
   int accumulator = 0;
-  arma::Mat<int> spins(size, size);
+  arma::Mat<double> spins(size, size);
 
   for(auto& spin : spins)
     spin = init == 0? (dist_uniform(rng) < 0.5 ? -1 : 1) : init;
 
   //cout << spins.at(-1, -1) << std::endl;
-  //double E = pro(spins);
+  double E = 0;
+  double m = arma::mean(arma::mean(spins));
+  for(size_t i = 0; i < size; i++)
+  for(size_t j = 0; j < size; j++)
+    E += proximity_energy(spins, i, j);
 
-  for(size_t k = 0; k < samples; k++){
-    int i = dist_spin_flip(rng);
-    int j = dist_spin_flip(rng);
+  for(size_t t = 0; t < samples; t++)
+  {
+    if(history != nullptr)
+    {
+      history->at(t, 0) = (E / pow(size, 2));
+      history->at(t, 1) = arma::mean(arma::mean(spins));
+      history->at(t, 2) = fabs(history->at(t, 1));
+    }
 
-    double delta_E = proximity_energy_difference(spins, i , j);//energy(spins) - E;
-    spins.at(i, j) *= -1;
-    //std::cout << "delta_E="<<delta_E << std::endl;
-    //keep spin flipped if ΔE<0 or random condition is met
-    spins.at(i, j) *= delta_E < 0 || dist_uniform(rng) < exp(-delta_E / kBT)? 1 : -1;
+    for(size_t k = 0; k < pow(size, 2); k++){
+      int i = dist_spin_flip(rng);
+      int j = dist_spin_flip(rng);
+      double delta_E = 2 * proximity_energy(spins, i, j);
+      //spin flipped if ΔE<0 or random condition is met
+      if(delta_E < 0 || dist_uniform(rng) < exp(-delta_E / kBT)) {
+        spins.at(i, j) *= -1;
+        E += delta_E;
+        m += 2 * spins.at(i, j) / pow(size, 2);
+      }
+    }
+  }
+
+  if(last != nullptr)
+  {
+    (*last)[1] = (E / pow(size, 2));;
+    (*last)[2] = arma::mean(arma::mean(spins));
+    (*last)[3] = fabs((*last)[2]);
   }
 
   return spins;
@@ -51,9 +77,43 @@ int main()
 {
   std::mt19937 rng;
   rng.seed(std::random_device()());
-  std::ofstream f("build/ising2d.txt");
+  uint schweeps = 1e3;
 
-  ising2d(rng, 1e8, 100, 1, 0).save("build/momentaufnahme.mat", arma::arma_ascii);
-  // cout << geildulo(101, 100);
+  arma::Mat<double> history(schweeps, 3);
+//  Anfangsbedingung alle Spins Zufällig gezogen
+  ising2d(rng, schweeps, 100, 1, 0, &history, nullptr).save("build/momentaufnahme_1_0.mat", arma::arma_ascii);
+  history.save("build/history_1_0.mat", arma::arma_ascii);
+  // ising2d(rng, schweeps, 100, 1.66, 0, &history, nullptr).save("build/momentaufnahme_1,66_0.mat", arma::arma_ascii);
+  // history.save("build/history_1,66_0.mat", arma::arma_ascii);
+  // ising2d(rng, schweeps, 100, 2.33, 0, &history, nullptr).save("build/momentaufnahme_2,33_0.mat", arma::arma_ascii);
+  // history.save("build/history_2,33_0.mat", arma::arma_ascii);
+  // ising2d(rng, schweeps, 100, 3, 0, &history, nullptr).save("build/momentaufnahme.mat_3_0", arma::arma_ascii);
+  // history.save("build/history_3_0.mat", arma::arma_ascii);
+
+// Anfangsbedingung alle Spins in eine Richtung
+history.save("build/history_1_0.mat", arma::arma_ascii);
+// ising2d(rng, schweeps, 100, 1.66, 1, &history, nullptr).save("build/momentaufnahme_1,66_1.mat", arma::arma_ascii);
+// history.save("build/history_1,66_1.mat", arma::arma_ascii);
+// ising2d(rng, schweeps, 100, 2.33, 1, &history, nullptr).save("build/momentaufnahme_2,33_1.mat", arma::arma_ascii);
+// history.save("build/history_2,33_1.mat", arma::arma_ascii);
+// ising2d(rng, schweeps, 100, 3, 1, &history, nullptr).save("build/momentaufnahme.mat_3_1", arma::arma_ascii);
+// history.save("build/history_3_1.mat", arma::arma_ascii);
+
+
+
+  uint T_steps = 100;
+  arma::Mat<double> results(T_steps, 4);
+  for(size_t i = 0; i < T_steps; i++)
+  {
+    double T = 1 + 2 / (double) T_steps * i;
+    auto row = results.row(i);
+    row[0] = T;
+    ising2d(rng, 300, 100, T, 0, nullptr, &row);
+  }
+
+  results.save("build/temperature.mat", arma::arma_ascii);
+
+  cout << results;
+
   return EXIT_SUCCESS;
 }
--- 3/2.py
+++ 3/2.py
@@ -1,7 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-mat = np.genfromtxt('build/momentaufnahme.mat', unpack=True, skip_header=2)
 
-plt.pcolormesh(mat, cmap='coolwarm')
-plt.savefig('build/momentaufnahme1.pdf')
+T=np.array(['1'])
+init=np.array(['0'])
+
+# T=np.array(['1','1.66','2.33','3'])
+# init=np.array(['0','1'])
+
+for index_T, value_T in enumerate(T):
+    for index_init, value_init in enumerate(init):
+        mat = np.genfromtxt("build/momentaufnahme_" + value_T + "_" + value_init +".mat", unpack=True, skip_header=2)
+        plt.pcolormesh(mat, cmap='cool', alpha=1)
+        plt.savefig("build/momentaufnahme_" + value_T + "_" + value_init +".pdf")
+        plt.close()
+        E, m, mm = np.genfromtxt("build/history_" + value_T + "_" + value_init +".mat", unpack=True, skip_header=2)
+        plt.plot(E,'o', alpha=0.7, markersize=0.5)
+        plt.plot(mm,'o', alpha=0.7, markersize=0.5)
+        plt.plot(m,'o', alpha=0.7, markersize=0.5)
+        plt.savefig("build/history_" + value_T + "_" + value_init +".pdf")
+        plt.close()
+
+T, E_T, m_T, absm_T = np.genfromtxt("build/temperature.mat", unpack=True, skip_header=2)
+plt.plot(T, m_T, 'o', label=r'$<m>(T)$', markersize=3, alpha=0.5)
+plt.plot(T, absm_T, 'o', label=r'$<|m|>(T)$', markersize=3, alpha=0.5)
+#plt.plot(T, E_T, 'o', label=r'E(T)', markersize=3)
+plt.axvline(2.27, label='Kritischer Punkt')
+plt.xlabel(r'$k_B T$')
+plt.legend(loc='best')
+plt.savefig('build/temperature.pdf')
