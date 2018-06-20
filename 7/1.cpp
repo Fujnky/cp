17cda1248814224f2739101842205677872e4dac
Lars Funke <lars.funke@udo.edu>
2018-06-20 12:48:59 +0200

    7 vorl

---
+++ 7/1.cpp
@@ -0,0 +1,42 @@
+//CP Blatt 7, Aufgabe 1. Dag-Björn Hering und Lars Funke: Euler & Runge-Kutta
+#include <iostream>
+#include <functional>
+
+using std::array;
+using std::function;
+
+template<uint dim>
+class ODESolver {
+public:
+    virtual array<double, dim> operator()(array<double, dim>) = 0;
+};
+
+template<uint dim>
+class EulerODESolver : public ODESolver<dim> {
+public:
+    explicit EulerODESolver(function<array<double, dim>(double, array<double, dim>)> f) {
+        //solve here or in seperate method?
+    };
+
+    array<double, dim> operator()(array<double, dim>) override {
+        //return solved function values
+    };
+};
+
+template<uint dim>
+class RungeKuttaODESolver : public ODESolver<dim> {
+public:
+    explicit RungeKuttaODESolver(function<array<double, dim>(double, array<double, dim>)> f) {
+        //solve here or in seperate method?
+    };
+
+    array<double, dim> operator()(array<double, dim>) override {
+        //return solved function values
+    };
+};
+
+int main() {
+    function<array<double, 3>(double, array<double, 3>)> f = [](double t, array<double, 3> y){return array<double, 3>({t,t,t});};
+    EulerODESolver<3> nummer1(f);
+	return EXIT_SUCCESS;
+}
